774ff6866b9a580a697623c7fe0166dae4a62a7c
Amber <andrade78932@hotmail.com>
2018-12-12 21:40:21 -0700

    work

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+import csv
+from collections import Counter
+with open ("03-Python_Homework_PyBank_Resources_budget_data.csv","r") as file: 
+    reader = csv.reader(file)
+    
+
+    for row in reader:
+      Counter("date"["total months"])
